642fb09b120e48f8f3dfa61e0109943acb082c68
Anuj Sharma <anujsharma877@gmail.com>
2020-07-12 20:25:54 +0530

    Added DB name and save record

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const User = require('../model/user');
 const { body, validationResult, check } = require('express-validator');
@@ -12,15 +13,31 @@ const router = express.Router();
  */
 router.post('/', [
     check('name','Please Add Name').not().isEmpty(),
-    check('email','Please Enter a vslid Email').isEmail(),
+    check('email','Please Enter a valid Email').isEmail(),
     check('password', "Please Enter a password with 6 or more characters").isLength({ min: 5 })
-],(req, res) =>{
+],async (req, res) => { 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
-    res.send("Passed");
-    // res.send(req.body);
+    const {name, email, password } = req.body;
+    try {
+        let user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({msg:"User Already Exists"});
+        }
+        user = new User({
+            name, email, password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password,salt);
+        await user.save();
+        res.send("User Saved Successfully");
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({msg:"Server Error"});
+    }
 });
 
 module.exports = router;
